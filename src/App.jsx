c9f94a4a5f3ef50b5f47f4f848b315918869d2dd
josejd2 <josejd2.jlv@gmail.com>
2025-03-28 14:48:36 +0100

    creación componente lista-peliculas ,usamos fetch y otros cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,12 @@
-import './App.css'
-import ListaUsuarios from './components/lista-usuarios/lista-usuarios'
-import coleccion from './data/peliculas.json'
+import "./App.css";
+import ListaPeliculas from "./components/lista-peliculas/lista-peliculas";
 
 function App() {
-
-  return (
+  return(
     <>
-    <div className='container'>
-      <h1>Catalogo de peliculas</h1>
-      <table className='table'>
-        <thead>
-          <tr>
-            <th>Nombre</th>
-            <th>Director</th>
-            <th>Generos</th>
-            <th>Año</th>
-          </tr>
-        </thead>
-        <tbody>
-        {coleccion.map((pelicula) =>(
-          <tr key={pelicula._id.$oid}>
-            <td>{pelicula.titulo}</td>
-            <td>{pelicula.director}</td>
-            <td>{pelicula.genero}</td>
-            <td>{pelicula.año}</td>
-          </tr>
-        ))}
-        </tbody>
-      </table>
-      <h2>Lista de usuarios</h2>
-      <ListaUsuarios/>
-    </div>
+      <ListaPeliculas/>
     </>
-  )
+  )    
 }
 
-export default App
+export default App;
---
+++ src/components/lista-peliculas/lista-peliculas.jsx
@@ -0,0 +1,50 @@
+import { useState, useEffect } from "react";
+
+const ListaPeliculas = () => {
+    const [peliculas, setPeliculas] = useState([]);
+
+    useEffect(() => {
+        fetch("/peliculas.json")
+            .then((res) => res.json())
+            .then((data) => setPeliculas(data))
+            .catch((error) => console.error("Error cargando JSON:", error));
+    }, []);
+
+    return (
+        <>
+            <div className="container">
+                <h1 className="text-center mb-4">Catalogo de peliculas</h1>
+                <div className="table-responsive">
+                    <table className="table table-bordered">
+                        <thead className="table-warning">
+                            <tr>
+                                <th scope="col">Titulo</th>
+                                <th scope="col">Director</th>
+                                <th scope="col">Año</th>
+                                <th scope="col">Generos</th>
+                                <th scope="col">Actores</th>
+                                <th scope="col">Puntuación</th>
+                                <th scope="col">Duración</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {peliculas.map((pelicula) => (
+                                <tr key={pelicula.id}>
+                                    <td>{pelicula.titulo}</td>
+                                    <td>{pelicula.director}</td>
+                                    <td>{pelicula.año}</td>
+                                    <td>{pelicula.generos.join(", ")}</td>
+                                    <td>{pelicula.actores.join(", ")}</td>
+                                    <td>{pelicula.puntuacion}</td>
+                                    <td>{pelicula.duracion}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default ListaPeliculas;
